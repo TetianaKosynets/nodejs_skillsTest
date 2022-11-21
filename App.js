f55bb4b18cba28712fb76407d371d413fd258850
TetianaKosynets <kch.tetiana@gmail.com>
2022-11-21 23:49:29 +1100

    input Category and Limit

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
+const prompt = require('prompt');
+
 const request = require('request');
-request('https://api.publicapis.org/entries', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var json = JSON.parse(body);
-    var entries = json.entries;
-    var entriesSorted = entries.sort((a, b) => a.API.normalize().localeCompare(b.API.normalize()));
-    var list = [];
-    entriesSorted.forEach(element => {
-        var apis = element.API;
-        list.push(apis);
-    });
-    var i = list.length-1;
-    var sortedList = [];
-    while (i > 0) {
-        sortedList.push(list[i]);
-        i--;
-      }
-    console.log(sortedList);
-  }
-})
+
+prompt.start();
+
+prompt.get(['Category', 'Limit'], function (err, result) {
+    if (err) { return onErr(err); }
+    console.log('Category: ' + result.Category);
+    console.log('Limit: ' + result.Limit);
+
+    request('https://api.publicapis.org/entries', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var json = JSON.parse(body);
+          var entries = json.entries;
+          var entriesSorted = entries.sort((a, b) => a.API.normalize().localeCompare(b.API.normalize()));
+          var list = [];
+          var allCategories = [];
+          entriesSorted.forEach(element => {
+              var apis = element.API;
+              list.push(apis);
+           
+              if (element.Category === result.Category)
+              allCategories.push(element.Category);
+          });
+
+          var limit = allCategories.length;
+          
+          var i = list.length-1;
+          var sortedList = [];
+          while (i > 0) {
+              sortedList.push(list[i]);
+              i--;
+            }
+          console.log(limit);
+        }
+
+        
+
+      });
+
+      
+    
+});
